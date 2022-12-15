20ddd2dd73a08c5fd45d2b97c9c58a6f1488e941
Matias Zommer <87200156+MZommer@users.noreply.github.com>
2022-12-15 15:02:36 -0300

    Added navbar for wallet login

--- Frontend/stockify/src/main.jsx
+++ Frontend/stockify/src/main.jsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+import { EtherProvider } from './contexts/EtherContext'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <EtherProvider>
+      <App />
+    </EtherProvider>
   </React.StrictMode>,
 )
