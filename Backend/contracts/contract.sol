924dfed0b8e486075e661ae6690b5a36eaf9029e
maurov <mvella@itba.edu.ar>
2022-12-15 18:23:26 -0300

    Refactor and bug fix

--- Backend/contracts/contract.sol
+++ Backend/contracts/contract.sol
@@ -9,64 +9,72 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "hardhat/console.sol";
 import "github.com/provable-things/ethereum-api/provableAPI.sol";
 
-struct deadLine {
+struct Objective {
     uint64 day;
-    uint16 percentage;
+    uint8 percentage;
+    bool achieved;
 }
-contract myContract is ERC721URIStorage, Ownable,
-    usingProvable  { 
-    using Counters for Counters.Counter;
-   Counters.Counter private _tokenIds;
-   // Its mapping a token to the company
-   address payable private ownerAddress;
-   uint32 private tokenPrize;
-   uint32 private tokenQty;
-   deadLine[] private deadLineList;
-   uint16 private index;
-   uint256 private balance;
-   uint16 index;
-   event LogObjectiveQuery(string description);
-   uint64 hasComplied;
+contract myContract is ERC721URIStorage, Ownable, usingProvable {
 
-   constructor(uint32 tokenPrize_, uint32 tokenQty_ ,deadLine[] memory deadLinesList_) ERC721("NFT", "ENFT") {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    // Its mapping a token to the company
+    address payable public ownerAddress;
+    uint32 public tokenPrice;  // add setter owneronly
+    uint32 private mintedPublic;
+    uint32 private mintedReserved; // add getter with authentication
+    uint32 private mintedTokens; // add getter
+
+    uint32 public MAX_SUPPLY; // add setter with auth
+    uint32 public RESERVED_SUPPLY; // add setter with auth
+
+    Objective[] private objectiveList;
+    uint256 private balance;
+
+    uint16 index;
+    event LogObjectiveQuery(string description);
+    uint64 hasComplied;
+
+   constructor(uint32 tokenPrice_, uint32 tokenQty_ , Objective[] memory objectiveList_) ERC721("NFT", "ENFT") {
         // Lo que haga es obtener la address de quien creo el contrato para luego
         // poder mandarle los fondos que obtuvieron de las tranferencias.
         hasComplied = 0;
-        tokenPrize = tokenPrize_;
-        tokenQty = tokenQty_;
-        deadLineList = deadLinesList_;
-        ownerAddress = payable(msg.sender);
+        tokenPrice = tokenPrice_;
+        mintedPublic = tokenQty_;
+        objectiveList = objectiveList_;
+        ownerAddress = address(msg.sender);
     }
 
     function totalSupply() public view returns (uint256) {
         return _tokenIds.current();
     }
 
-   function mintNFT(address recipient, string memory tokenURI) public payable returns (uint256){
-        require(msg.balance == tokenPrize);
-        balance += msg.balance;
-       _tokenIds.increment();
-       uint256 newItemId = _tokenIds.current();
-       _mint(recipient, newItemId);
-       _setTokenURI(newItemId, tokenURI);
-       return newItemId;
-   }
+    function mintNFT(address recipient, string memory tokenURI) public payable returns (uint256){
+        require(msg.value == tokenPrice);
+        balance += msg.value;
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        return newItemId;
+    }
     //the owner can withdraw from the contract because payable was added to the state variable above
     function withdraw (uint _amount) public onlyOwner {
-        require(msg.sender <= balance);
-        require(deadLineList[index].day <= block.timestamp);
-        if(index == deadLineList.length - 1)
+        require(msg.balance <= balance);
+        require(objectiveList[index].day <= block.timestamp);
+        if(index == objectiveList.length - 1) {
             ownerAddress.transfer(balance);
             balance = 0;
-        else{
+        }
+        else {
             //TODO chequear tema decimales
             //transfer(ownerAddress, balance * deadlineList[index].percentage) * (10 ** 2))
-            uint256 toTransfer = (deadLineList[index].percentage * balance) / 100;
+            uint256 toTransfer = (objectiveList[index].percentage * balance) / 100;
             ownerAddress.transfer(toTransfer);
             balance -= toTransfer;
             index++;
         }
-
     }
 
     function transferFromPayable(
--- Backend/contracts/contractFacotory.sol
+++ Backend/contracts/contractFacotory.sol
@@ -5,9 +5,8 @@ contract ContractFactory {
 
     event ContractDeployed(address indexed creator, address indexed NFTAddress);
 
-    function createContract(string memory name,uint32 tokenPrize_, uint32 tokenQty_ ,deadLine[] memory deadLinesList_) public  {
+    function createContract(string memory name, uint32 tokenPrize_, uint32 tokenQty_ , deadLine[] memory deadLinesList_) public  {
         myContract contractDeployed = new myContract(tokenPrize_, tokenQty_, deadLinesList_);
         emit ContractDeployed(msg.sender, address(contractDeployed));
-        
     }   
 }
