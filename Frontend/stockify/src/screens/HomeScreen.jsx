3340d88760866425eed3beeb1a8ccfc90c1f6c64
tgaybare <tomigayba02yt@gmail.com>
2022-12-15 19:46:58 -0300

    ProjectScreen progress

--- Frontend/stockify/src/screens/HomeScreen.jsx
+++ Frontend/stockify/src/screens/HomeScreen.jsx
@@ -1,10 +1,9 @@
+import '../app.css'
 export default function HomeScreen() {
 
     return (
-        <div className="container">
-            <header className="App-header">
-                <h1 className="Title">Stockify</h1>
-            </header>
+        <div>
+            <h1>jakdf;ljkadfk</h1>
         </div>
     )
 }
--- Frontend/stockify/src/screens/ProjectScreen.jsx
+++ Frontend/stockify/src/screens/ProjectScreen.jsx
@@ -1,13 +1,48 @@
 import { useParams } from 'react-router-dom';
 
-
+import '../app.css'
 import { useEtherContext } from '../contexts/EtherContext';
 import styled from 'styled-components';
+import React from "react";
+import Grid from "@mui/material/Grid";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import {Button} from "@mui/material";
 
 export default function ProjectScreen() {
     const { projectId } = useParams();
 
-    return ( <></>)
+    return (
+        <Wrapper>
+        <div>
+            <h3 className="App-title">Krusty Crab</h3>
+                <div className="container">
+                    <Carousel className="App-image">
+                        <div>
+                            <img src="/kc2.jpg" alt="krusty crab"/>
+                        </div>
+                        <div>
+                            <img src="/kc.png" alt="krusty crab 2"></img>
+                        </div>
+                    </Carousel>
+                    <Button className="App-button" variant="contained">Buy</Button>
+                    <h5 className="App-projecttext">Available shares: 145</h5>
+                </div>
+        </div>
+        </Wrapper>
+    )
 }
 
-const Wrapper = styled.div``
+const Wrapper = styled.div`
+  .container {
+    display: flex;align-items: center;
+  }
+  .button{
+    background-color: limegreen;
+    color: black;
+    margin-left: 15%;
+    margin-bottom: 10%;
+    display: inline-flex;
+    align-content: center;
+  }
+ `
