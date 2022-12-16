44692b3ffdc9cab592c8e56b29d3d88d13a78b5a
Martin Hexht <mhecht@itba.edu.ar>
2022-12-16 01:02:12 -0300

    removal of project index

--- Backend/contracts/masterContract.sol
+++ Backend/contracts/masterContract.sol
@@ -4,25 +4,26 @@
 //-poder hacer withdraw de cada uno de los contratos
 //-lockear el withdraw de los contratos hasta cierto timestamp
 
-contract myMaster {//TODO: cambiar todos los mappings sacar projectId
+contract myMaster {
     mapping(address => address) private mapPayeeWithContract; // Payee -> Contract
     mapping(address => uint64) private mapPayeeWithAmount; // Payee -> Amount
     mapping(address => bool) private mapContractWithCanClaim ; // can claim
 
-    mapping(address => uint64) private mapProjectIdxToAmount; // ProjectID -> Amount
-    mapping(uint32 => uint64) private mapProjectIdxToDuration; // ProjectID -> Duration
-    mapping(uint32 => uint64) private mapProjectIdxToStart //  ProjectID -> Start
+    mapping(address => uint64) private mapContractToAmount; // ProjectID -> Amount
+    mapping(address => uint64) private mapContractToDuration; // ProjectID -> Duration
+    mapping(address => uint64) private mapContractToStart //  ProjectID -> Start
 
-    mapping(uint32 => address) private mapProjectIdxToAddress; // ProjectID -> ProjectAddress
-    mapping(uint32 => address) private mapProjectIdxToBeneficiary; // ProjectID -> OwnerAddress    //mapping(address => uint32[]) private mapBeneficiaryToPreject; // OwnerAddress -> ProjectID
+    mapping(address => address) private mapContractToBeneficiary; // ProjectID -> OwnerAddress
+
+    //creo que esta esta demas, para que sirve? seria address[] no?
     mapping(address => uint32[]) private mapBeneficiaryProjectList;
 
     function deployProject() public {
 
     }
 
-    function beneficiary(uint32 projectIdx_) public view virtual returns (address) {
-        return mapProjectIdxToAddress[projectIdx_];
+    function beneficiary(address contractAddress) public view virtual returns (address) {
+        return mapContractToBeneficiary[contractAddress];
     }
 
     function canClaim(address contractAddress) public view virtual returns (uint256) {
@@ -42,8 +43,11 @@ contract myMaster {//TODO: cambiar todos los mappings sacar projectId
 
     // Recibir el address del ERC20 como parametro, que lo deployee otro
     function addBeneficiary(IERC20 address) public returns (address){
-        mapProjectIdxToAddress[address] = New erc20();// hay un quilombo con los mappings
-        mapProjectIdxToBeneficiary = ERC20(address).getBeneficiary();
+
+        //que devuelve un erc20 ? a que lo quisieron mappear aca? estos mappings estan al reves
+        mapProjectIdxToAddress[address] = New erc20();
+
+        mapContractToBeneficiary[address] = ERC20(address).getBeneficiary();
         //TODO check amount
 
 
@@ -52,9 +56,11 @@ contract myMaster {//TODO: cambiar todos los mappings sacar projectId
     function buyToken(uint16 adressWallet) payable{
         //require(msg.value > 4 eth);
         //necesito calcular el 4% , cuanto representa cada token de la empresa
-        require(ERC20(mapProjectIdxToAddress[adressWallet]).canTransfer(msg.value);)
-        //Aca debo sumar la plata que tiene la empresa
 
+        //esta linea no tiene sentido
+        require(ERC20(mapProjectIdxToAddress[adressWallet]).canTransfer(msg.value);)
+
+        //Aca debo sumar la plata que tiene la empresa. esta tampoco tiene sentido
         ERC20(mapProjectIdxToAddress[adressWallet]).transferValue(addressWallet, msg.value);
 
     }
@@ -67,14 +73,13 @@ contract myMaster {//TODO: cambiar todos los mappings sacar projectId
         SafeERC20.safeTransfer(IERC20(token), beneficiary(), amount);
     }
 
-    function releaseEth(uint32 projectIdx_) public {
-        if ( block.now  > mapProjectIdxToDuration[projectIdx_] + mapProjectIdxToDuration[projectIdx_]){
+    function releaseEth(address contractAddress) public {
+        if ( block.now  > mapContractToDuration[contractAddress] + mapContractToStart[contractAddress]){
             if(/* oracle approves*/){
                 //TODO: chequeo de permisos
-                Address.sendValue(payable(beneficiary(projectIdx_)), mapProjectIdxToAmount[projectIdx_]);
+                Address.sendValue(payable(beneficiary(contractAddress)), mapContractToAmount[contractAddress]);
             }else {
-                //allow claim back
-                mapContractWithCanClaim[mapProjectIdxToAddress[projectIdx_]] = true;
+                mapContractWithCanClaim[contractAddress] = true;
             }
         }
     }
