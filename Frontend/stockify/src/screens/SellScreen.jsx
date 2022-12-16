2c4dae4d0ced2efddedac4f49c7257e9df454632
tgaybare <tomigayba02yt@gmail.com>
2022-12-16 16:45:28 -0300

    SellScreen styling

--- Frontend/stockify/src/screens/SellScreen.jsx
+++ Frontend/stockify/src/screens/SellScreen.jsx
@@ -1,6 +1,7 @@
 import { useEtherContext } from '../contexts/EtherContext';
 import styled from 'styled-components';
 import {TextField, InputAdornment, Button, Container } from "@mui/material";
+import Grid from "@mui/material/Grid";
 
 export default function SellScreen() {
     const { EtherHelper } = useEtherContext()
@@ -13,21 +14,37 @@ export default function SellScreen() {
     }
     return (
         <Wrapper>
-          <Container>
-            <h1 className="title t"> Sell your Stock</h1>
-            <form onSubmit={mint}>
-              <div className="container">
-                  <h4 className="t">What percentage of your company are you willing to mint?</h4>
-                  <TextField variant="outlined" type="number" max="100" min="0" name="perc"/>
-                  <h4 className='t'>To what valuation?</h4>
-                  <TextField variant="outlined" type="number" min="0" name="valuation"/>
-                  <h4 className='t'>How many tokens do you want to mint?</h4>
-                  <TextField variant="outlined" type="number" min="0" name="tokenAmount"/>
-              </div>
-              <Button className="button" type="submit">Mint</Button>
-            </form>
-              
-          </Container>
+              <Grid item xs={6}>
+                  <h1 className="title t"> Sell your Stock</h1>
+                  <form onSubmit={mint}>
+                      <div className="container">
+                          <Grid container spacing={2}>
+                              <Grid item xs={6}>
+                                  <h4 className="t">What percentage of your company are you willing to mint?</h4>
+                                  <TextField variant="outlined" type="number" max="100" min="0" name="percentage" className="text-field"/>
+                                  <h4 className='t'>To what valuation?</h4>
+                                  <TextField variant="outlined" min="0" name="valuation" className="text-field"/>
+                                  <h4 className='t'>How many tokens do you want to mint?</h4>
+                                  <TextField variant="outlined" type="number" min="0" name="tokenAmount" className="text-field"/>
+                                  <Button className="button" type="submit">Mint</Button>
+                              </Grid>
+                              <Grid item xs={6} className="name">
+                                  <h4 className="t">Description of your company</h4>
+                                  <TextField multiline maxRows={4} variant="outlined" type="text"  name="description" className="text-field"/>
+                                  <h4 className='t'>What is your objective?</h4>
+                                  <TextField multiline maxRows={4} variant="outlined" type="text" name="objective" className="text-field"/>
+                                  <h4 className='t'>What is the deadline?</h4>
+                                  <TextField variant="outlined" type="date"  name="deadline" className="text-field"/>
+                              </Grid>
+                          </Grid>
+
+
+                      </div>
+
+                  </form>
+              </Grid>
+
+
         </Wrapper>
     )
 }
@@ -38,9 +55,15 @@ const Wrapper = styled.div`
     margin-top: 2%;
     margin-left: 12%;
   }
+  
+  .text-field{
+    background-color: white;
+  }
 
   .title{
     font-size: 40px;
+    margin-left: 10%;
+    margin-top: 5%;
   }
 
   .t{
@@ -51,10 +74,13 @@ const Wrapper = styled.div`
   .button{
     background-color: limegreen;
     color: black;
-    margin-left: 12%;
-    margin-top: 1%;
+    margin-top: 3%;
     padding-left: 5%;
     padding-right: 5%;
     align-content: center;
+    display:block;
+  }
+  .end{
+    margin-bottom: 10%;
   }
  `
