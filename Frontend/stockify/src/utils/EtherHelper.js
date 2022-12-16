3ebdea0c95f34576368fa27d82b72d4d702f3035
Matias Zommer <87200156+MZommer@users.noreply.github.com>
2022-12-16 16:00:05 -0300

    Update EtherHelper.js

--- Frontend/stockify/src/utils/EtherHelper.js
+++ Frontend/stockify/src/utils/EtherHelper.js
@@ -1,6 +1,9 @@
 import { ethers } from "ethers";
 
-import { RPC_URL } from "../contants";
+import { RPC_URL, ProjectManagerAddress } from "../contants";
+
+import Project_sol from "./contracts/Project.json"
+
 
 export default class EtherHelper {
     constructor (web3Provider = null) {
@@ -10,5 +13,20 @@ export default class EtherHelper {
             this.etherProvider = new ethers.providers.JsonRpcProvider(RPC_URL);
         
         this.isLogged = Boolean(web3Provider);
+    }
+
+    async createProject(metadata) {
+        const signer = this.etherProvider.getSigner()
+        const Project = new ethers.ContractFactory(Project_sol.ABI, Project_sol.bytecode, signer)
+        
+        return new Promise(async (resolve, reject) => {
+            await Project.deploy(
+                signer.address, ProjectManagerAddress, 
+                metadata.name, metadata.symbol, metadata.premint, metadata.equityValue, metadata.equity)
+                .catch(err => reject(err))
+            Project.deployed()
+                .then(() => resolve(Project))
+                .catch(err => reject(err))
+        })
     }
 }
